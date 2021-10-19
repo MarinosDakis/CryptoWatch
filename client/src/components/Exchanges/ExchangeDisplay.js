9ae032c26b7dd744068b0a1949fe400e340dc1c1
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-10-19 18:16:46 -0400

    edited exchanges component

--- client/src/components/Exchanges/ExchangeDisplay.js
+++ client/src/components/Exchanges/ExchangeDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid, Card, Zoom } from '@material-ui/core';
+import { Typography, Grid, Card, Zoom, Container, Divider, ListItemText } from '@material-ui/core';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -19,26 +19,42 @@ export default function ExchangeDisplay() {
          <Grid container className={classes.container}>
 
                     {exchangeList.map((item, index) => (
-                        <Card sx={{ maxWidth: 400 }} className={classes.card}>
-                        <Typography  gutterBottom variant="h4" align="center">{item.name}</Typography>
-                        <img src={item.img}></img>
-                            <CardMedia className={classes.img} component="img" height="75px" image={`"${item.logo}`} alt={item.name ? `${item.name}-logo` : "exchange-logo"} />
+                    <Container key={item.name} className={classes.container}>
+                        <Card className={classes.card} align="center">
+                            { /* adapted from: https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react#:~:text=Inside%20public%20folder%20create%20an%20assets%20folder%20and%20place%20image%20path%20accordingly. */}
+                            <CardMedia className={`${classes.img} ${classes.root}`} component="img" image={`${process.env.PUBLIC_URL + item.logo}`} alt={item.name ? `${item.name}-logo` : "exchange-logo"} />
                             <CardContent>
-                                <Typography component="legend">Rating</Typography>
-                                <Rating name="read-only" value={item.rating} readOnly />
+                                <Typography className={classes.text} variant="h5" component="legend">Rating</Typography>
+                                <Rating value={item.rating} size="large" readOnly />
+                                <Divider className={classes.root} />
+                                <Typography className={classes.text} variant="h5" component="legend">Tradeable Cryptocurrencies</Typography>
+                                <Typography className={classes.text} variant="h6">{item.cryptoAmount}</Typography>
+                                <Divider className={classes.root} />
+                                <Typography className={classes.text} variant="h5" component="legend">Fee (Maker/Taker)</Typography>
+                                <Typography className={classes.text} variant="h6">{item.fee}</Typography>
+                                <Divider className={classes.root} />
+                                <Typography className={classes.text} variant="h5" component="legend">Benefits</Typography>
+                                <Typography className={classes.text} variant="h6">{item.fee}</Typography>
+                                <Divider className={classes.root} />
+                                <Typography className={classes.text} variant="h5" component="legend">Drawbacks</Typography>
+                                <Typography className={classes.text} variant="h6">{item.fee}</Typography>
+                                <Divider className={classes.root} />
                             </CardContent>
                             <CardActions display="flex">
                                 <Grid container>
                                     <Grid item xs align="center">
                                         <a className={classes.url} href={item.url} target="_blank" rel="noreferrer">
                                             <WebIcon />
-                                            <Typography>Visit Exchange</Typography>
+                                            <Typography>Exchange Website</Typography>
                                         </a>
                                     </Grid>
                                 </Grid>
                             </CardActions>
+                            <Typography variant="caption">Source: Forbes</Typography>
                         </Card>
+                    </Container>
                     ))}
+
                 </Grid>
             </Zoom>
     )
--- client/src/components/Exchanges/styles.js
+++ client/src/components/Exchanges/styles.js
@@ -1,12 +1,23 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+
+root: {
+    '&.MuiCardMedia-root': {
+        objectFit: "scale-down",
+    },
+    '&.MuiDivider-root': {
+        borderColor: "#857687",
+        borderWidth: "1.5px",
+      },
+},
   card: {
     backgroundColor: "white",
-    padding: theme.spacing(2),
+    padding: theme.spacing(3),
     margin: "20px 0",
-    width: 400,
-    borderRadius: 45,
+    width: 500,
+    height: "auto",
+    borderRadius: 15,
     border: "#7e7878 solid 1px",
     boxShadow: "0 4px 5px 5px rgba(0, 0, 0, .2)", 
   },
@@ -17,5 +28,13 @@ export default makeStyles((theme) => ({
   container: {
     display: "flex",
     justifyContent: "center",
+  },
+  img: {
+      height: "200px",
+      width: "300px",
+  },
+  text: {
+      fontFamily: "Arial",
+      marginTop: 5,
   }
 }));
